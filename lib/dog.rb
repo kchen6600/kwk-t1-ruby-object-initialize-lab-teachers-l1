139bd62dfc4930691da1724e9691fe9685713711
Karen Chen <kchen6600@gmail.com>
2018-07-18 00:57:12 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+
+class Dog
+  def name
+    @name
+  end
+
+  def name=(namein)
+    @name = namein
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breedin)
+    @breed = breedin
+  end
+end
